e242a60eda780730f043165bbf83c230311b0ccb
Black <black@relayx.com>
2023-02-02 01:44:59 +0400

    onboarding prompt

--- src/inject.ts
+++ src/inject.ts
@@ -29,11 +29,13 @@ window.addEventListener('message', (message: MessageEvent) => {
     }
 })
 
+let authRedirectUrl: string | undefined = void 0
 
 async function checkAndCall<T>(method: string, params: T): Promise<any> {
     const linked = await RelayOneClient.isLinked();
     if (!linked) {
-        await send("onboard", {})
+        showSignupPopup(authRedirectUrl || window.location.toString())
+        return new Promise(() => { });
     }
     const result = await send(method, params);
     if (result.error) {
@@ -50,6 +52,7 @@ async function unLinkedCall<T>(method: string, params: T): Promise<any> {
     return result;
 }
 
+
 const RelayOneClient = {
     p2p: true,
     extension: true,
@@ -57,7 +60,7 @@ const RelayOneClient = {
         if (typeof url !== "string") {
             return false;
         }
-        // RELAY_STATE.authRedirect = url;
+        authRedirectUrl = url;
         return true;
     },
 
@@ -159,3 +162,121 @@ const RelayOneClient = {
 console.log("INJECTED");
 
 (window as any)['relayone'] = RelayOneClient
+
+const image = require('data-url:./logo-header.png')
+const POPUP = `
+<div style="
+margin: 0 auto; padding: 20px; width: 300px; min-height: 250px; 
+background: rgb(47, 59, 82);border-radius: 10px;
+font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-serif; display: flex; flex-direction: column;
+justify-content: space-between;
+box-sizing: border-box;
+position: relative;
+">
+<img
+  style="
+    width: 120px;
+    height: 20px;
+    display: block;
+    margin: 20px auto;
+  "
+  src="${image}"
+  alt=""
+/>
+<div
+  style="
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    cursor: pointer;
+    boxSizing: content-box;
+  "
+  class="relay-one-close"
+>
+  <span
+    style="
+      display: block;
+      box-sizing: content-box;
+      cursor: pointer;
+      background: rgb(101, 125, 149);
+      border-radius: 50%;
+      width: 16px;
+      height: 16px;
+      text-align: center;
+      padding: 2px;
+      font-size: 12px;
+      color: rgb(17, 26, 44);
+      line-height: 16px;
+    "
+  >
+    ✕
+  </span>
+</div>
+<div
+  style="
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  "
+>
+  <div
+    style="
+      color: white;
+      font-size: 16px;
+      width: 197px;
+      text-align: center;
+    "
+  >
+    Please sign in with RelayX to enable spending
+  </div>
+  <a
+    style="
+      color: white;
+      display: block;
+      box-sizing: content-box;
+      text-decoration: none;
+      background-color: rgb(38, 105, 255);
+      padding: 10px;
+      border-radius: 5px;
+      width: 130;
+      margin-top: 10;
+      text-align: center;
+    "
+    rel="noopener noreferrer"
+    href="#"
+  >
+    Sign in
+  </a>
+</div>
+<div style="margin: 0 auto; width: 200px"></div>
+</div>
+`
+
+function showSignupPopup(redirectUrl: string) {
+    const popup = document.createElement('div')
+    popup.setAttribute('style', `
+   display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color = rgb(0, 0, 0, 0.2);
+  z-index = 99999;
+    `)
+    popup.innerHTML = POPUP;
+    const a = popup.querySelector('a')!
+    a.addEventListener('click', () => {
+        window.location = `https://relayx.com/wallet/auth#${redirectUrl}`
+    })
+    const close = popup.querySelector('.relay-one-close')!
+    close.addEventListener('click', () => {
+        document.body.removeChild(popup)
+    })
+
+    document.body.appendChild(popup)
+}
